99a6bd77cf6975e3700bb4eb4bbe967e824a4cc1
고범석 <55486001+kobums@users.noreply.github.com>
2024-07-05 17:20:12 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.go
@@ -0,0 +1,19 @@
+func solution(angle int) int {
+    if 0 < angle && angle < 90 {
+        return 1
+    }
+    
+    if angle == 90 {
+        return 2
+    }
+    
+    if 90 < angle && angle < 180 {
+        return 3
+    }
+    
+    if angle == 180 {
+        return 4
+    }
+    
+    return 0
+}
