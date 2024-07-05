0ca0beb77b4d91fd8f0155f6d51ac35f24e6fc6e
고범석 <55486001+kobums@users.noreply.github.com>
2024-07-05 19:02:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 35.85 ms, Memory: 66.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,23 @@
+function solution(participants, completions) {
+    const myMap = new Map();
+
+    for ( const participant of participants){
+        if(!myMap.get(participant)){
+            myMap.set(participant, 1);
+        }else{
+            myMap.set(participant, myMap.get(participant)+1);
+        }
+    }
+
+    for(const completion of completions){
+        if(myMap.get(completion)){
+            myMap.set(completion, myMap.get(completion)-1);
+        }
+    }
+    
+    for(const participant of participants){
+        if(myMap.get(participant) && myMap.get(participant) >=1 ){
+            return participant;
+        }
+    }
+}
